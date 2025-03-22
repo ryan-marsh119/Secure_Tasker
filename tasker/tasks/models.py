35f760b963f8716f592b6cd4dd4d7bb63dec542f
rma1265 <r.marsh9619@yahoo.com>
2025-03-22 15:59:43 -0400

    Added Postgresql to the program and added data via psql in comand prompt. Debugged created_at field in tasks/models.py since auto_now_add was not working. Needed extra paramater "null=True".

--- tasker/tasks/models.py
+++ tasker/tasks/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    # test = models.DateField(auto_created=True)
+
+    def __str__(self):
+        return self.title
--- tasker/tasks/views.py
+++ tasker/tasks/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import TaskSerializer
-# from .models import Task
+from .models import Task
 
 
 # task1 = Task(title='task 1', description='code through the sleepyness', completed=False, created_at='2025-03-17')
@@ -11,10 +11,11 @@ class TaskListView(APIView):
 
     def get(self, request):
         # Hard coded tasks for milestone 1
-        task1 = {'title':'task 1', 'description':'code through the sleepyness', 'completed':False, 'created_at':'2025-03-17'}
-        task2 = {'title':'task 2', 'description':'go to bed early', 'completed':False, 'created_at':'2025-03-18'}
+        # task1 = {'title':'task 1', 'description':'code through the sleepyness', 'completed':False, 'created_at':'2025-03-17'}
+        # task2 = {'title':'task 2', 'description':'go to bed early', 'completed':False, 'created_at':'2025-03-18'}
         
-        tasklist = [task1, task2]
+        # Getting objects stored in database. 
+        tasklist = Task.objects.all()
         serializer = TaskSerializer(tasklist, many=True)
 
         return Response(serializer.data)        
