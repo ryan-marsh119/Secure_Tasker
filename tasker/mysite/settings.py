f30c58da58a40822efef7c633794812375b1c4ea
Ryan <ryan.marsh119@gmail.com>
2025-05-11 14:58:10 -0400

    Prepared Django backend for Render deployment.

--- tasker/mysite/settings.py
+++ tasker/mysite/settings.py
@@ -30,7 +30,7 @@ FIELD_ENCRYPTION_KEY = env.str("ENCRYPTION_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
