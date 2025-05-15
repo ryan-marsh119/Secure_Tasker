24fd84ad67c06a7f77bcb0d0ec1d213d50fdd680
Ryan <ryan.marsh119@gmail.com>
2025-05-15 19:39:35 -0400

    Added a README file.

--- tasker/mysite/settings.py
+++ tasker/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 FIELD_ENCRYPTION_KEY = env.str("ENCRYPTION_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
